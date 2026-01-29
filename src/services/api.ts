584e6b1a45e3049b56ed31a85265d073f03234ab
ryangamerbr27-debug <ryangamerbr27@gmail.com>
2026-01-29 16:05:10 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,3 +2,19 @@ export async function getGastos() {
   const res = await fetch('/api/get_gastos')
   return res.json()
 }
+
+export async function createGasto(gasto: {
+  descricao: string
+  data: string
+  categoria: string
+  metodo_pagamento: string
+  valor: number
+}) {
+  const res = await fetch('/api/create_gasto', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(gasto)
+  })
+
+  return res.json()
+}
